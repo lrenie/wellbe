54f033c5cca9ac5ac66d053e3ca8ea45bc707329
Corentin Lumineau <lumineau.corentin@gmail.com>
2020-08-25 15:15:54 +0200

    Validations pour warm-up

--- app/models/warm_up.rb
+++ app/models/warm_up.rb
@@ -1,4 +1,8 @@
 class WarmUp < ApplicationRecord
   belongs_to :body_area
   has_many :sessions, dependent: :destroy
+
+  validates :name, :time, :kcal, presence: true
+  validates :name, uniqueness: true
+
 end
