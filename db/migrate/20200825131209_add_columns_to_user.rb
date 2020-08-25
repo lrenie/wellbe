9ac2fb98383e792ab5d69522225d12504123ff22
Corentin Lumineau <lumineau.corentin@gmail.com>
2020-08-25 15:43:12 +0200

    Debugging Migration Table User

--- db/migrate/20200825131209_add_columns_to_user.rb
+++ db/migrate/20200825131209_add_columns_to_user.rb
@@ -1,7 +1,7 @@
 class AddColumnsToUser < ActiveRecord::Migration[6.0]
   def change
-    add_column :user, :first_name, :string
-    add_column :user, :last_name, :string
-    remove_column :user, :name, :string
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    remove_column :users, :name, :string
   end
 end
