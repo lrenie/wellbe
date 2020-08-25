d00103f30f9023cedf6be48ac6a8a813e6bd7b71
benoit graziani <benoit.c.graziani@hotmail.fr>
2020-08-25 15:18:43 +0200

    validates model exercise

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,4 +1,10 @@
 class Exercise < ApplicationRecord
   belongs_to :body_area
   has_many :sessions, dependent: :destroy
+
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :time, presence: true
+  validates :diff_coef, presence: true
+  validates :kcal, presence: true
 end
