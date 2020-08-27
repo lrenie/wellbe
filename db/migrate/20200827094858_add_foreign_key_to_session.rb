6826a38744d533952f71a73fdec6ef7991b4fcc5
Corentin Lumineau <lumineau.corentin@gmail.com>
2020-08-27 15:33:55 +0200

    commit

--- db/migrate/20200827094858_add_foreign_key_to_session.rb
+++
@@ -1,5 +0,0 @@
-class AddForeignKeyToSession < ActiveRecord::Migration[6.0]
-  def change
-    add_reference :sessions, :session_exercise, index: true 
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_27_094858) do
+ActiveRecord::Schema.define(version: 2020_08_27_094749) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -91,8 +91,6 @@ ActiveRecord::Schema.define(version: 2020_08_27_094858) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "session_exercise_id"
-    t.index ["session_exercise_id"], name: "index_sessions_on_session_exercise_id"
     t.index ["user_id"], name: "index_sessions_on_user_id"
   end
 
