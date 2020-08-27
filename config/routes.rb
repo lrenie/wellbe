fde21e59f2fcb9a6bcb7977c3fcafbe5fc746633
Eugenie Avedikian <eugenieavedikian@gmail.com>
2020-08-27 15:34:39 +0200

    debut de la show de session

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :users, only: [:show] do
-    get '/sessions/new', to: 'sessions#new', as: 'new_session'
-    get '/sessions/:id', to: 'sessions#show', as: 'my_session'
+  resources :users, only: [:show]
     # resources :sessions, only: [ :new, :show]
-  end
+  resources :seesions, only: :create
+  get '/sessions/new', to: 'sessions#new', as: 'new_session'
+  get '/sessions/:id', to: 'sessions#show', as: 'my_session'
   get '/sessions/choice', to: 'sessions#choice'
 end
 
