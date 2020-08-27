28072e6551407af2173a917f39cd0e625e2b73a1
Louis Renie <louis.renie@hotmail.com>
2020-08-27 18:48:16 +0200

    new de session

--- app/models/body_area.rb
+++ app/models/body_area.rb
@@ -1,7 +1,7 @@
 class BodyArea < ApplicationRecord
-  
+  BODY_AREAS = ["Général", "Haut du corps", "Bas du corps"]
+
   has_many :exercises
-  
 
   validates :name, uniqueness: true, presence: true
 end
--- app/models/session.rb
+++ app/models/session.rb
@@ -1,9 +1,12 @@
 class Session < ApplicationRecord
+  DIFFICULTY = ["Débutant", "Intermédiaire", "Avancé", "Confirmé"]
+
   belongs_to :user
 
   has_many :session_exercises, dependent: :destroy
   # has_many :users, through: :session_participant, dependent: :destroy
   has_many :session_participants, dependent: :destroy
+
 
   validates :difficulty, presence: true
   validates :mode, presence: true
