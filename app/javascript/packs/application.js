65e43dfeaa3e00e6db21841a794e2eb9749a1533
corentin-lumineau <63396946+corentin-lumineau@users.noreply.github.com>
2020-09-02 12:24:24 +0200

    Update application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -45,46 +45,6 @@ document.addEventListener('turbolinks:load', () => {
   innitFlatPicker();
   addFriendsParticipants();
 
-
-  if (document.querySelector(".chrono-area")) {
-    const twilioToken = document.getElementById('local-media').dataset.twilioToken
-    const roomName = document.getElementById('local-media').dataset.roomName
-    connect(twilioToken, { name: roomName }).then(room => {
-
-      console.log(`Successfully joined a Room: ${room}`);
-      console.log(roomName)
-      const { createLocalVideoTrack } = require('twilio-video');
-
-        createLocalVideoTrack().then(track => {
-          const localMediaContainer = document.getElementById('local-media');
-          localMediaContainer.appendChild(track.attach());
-        });
-
-      const localParticipant = room.localParticipant;
-
-      console.log(`Connected to the Room as LocalParticipant "${localParticipant.identity}"`);
-
-      room.on('participantConnected', participant => {
-        console.log(`A remote Participant connected: ${participant}`);
-      });
-
-      room.participants.forEach(participant => {
-        participant.tracks.forEach(publication => {
-          if (publication.track) {
-            document.getElementById('remote-media-div').appendChild(publication.track.attach());
-          }
-        });
-
-       participant.on('trackSubscribed', track => {
-          document.getElementById('remote-media-div').appendChild(track.attach());
-        });
-      });
-
-    },
-      error => {
-      console.error(`Unable to connect to Room: ${error.message}`);
-    });
-  }
 });
 
 
