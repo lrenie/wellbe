9d0d427cb23d2b1ca20cd3d5e84685543c18693a
Louis Renie <louis.renie@hotmail.com>
2020-09-02 11:47:01 +0200

    cards previous done

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -46,44 +46,45 @@ document.addEventListener('turbolinks:load', () => {
   addFriendsParticipants();
 
 
+  if (document.querySelector(".chrono-area")) {
+    const twilioToken = document.getElementById('local-media').dataset.twilioToken
+    const roomName = document.getElementById('local-media').dataset.roomName
+    connect(twilioToken, { name: roomName }).then(room => {
 
-  const twilioToken = document.getElementById('local-media').dataset.twilioToken
-  const roomName = document.getElementById('local-media').dataset.roomName
-  connect(twilioToken, { name: roomName }).then(room => {
-    
-    console.log(`Successfully joined a Room: ${room}`);
-    console.log(roomName)
-    const { createLocalVideoTrack } = require('twilio-video');
+      console.log(`Successfully joined a Room: ${room}`);
+      console.log(roomName)
+      const { createLocalVideoTrack } = require('twilio-video');
 
-      createLocalVideoTrack().then(track => {
-        const localMediaContainer = document.getElementById('local-media');
-        localMediaContainer.appendChild(track.attach());
+        createLocalVideoTrack().then(track => {
+          const localMediaContainer = document.getElementById('local-media');
+          localMediaContainer.appendChild(track.attach());
+        });
+
+      const localParticipant = room.localParticipant;
+
+      console.log(`Connected to the Room as LocalParticipant "${localParticipant.identity}"`);
+
+      room.on('participantConnected', participant => {
+        console.log(`A remote Participant connected: ${participant}`);
       });
-    
-    const localParticipant = room.localParticipant;
 
-    console.log(`Connected to the Room as LocalParticipant "${localParticipant.identity}"`);
+      room.participants.forEach(participant => {
+        participant.tracks.forEach(publication => {
+          if (publication.track) {
+            document.getElementById('remote-media-div').appendChild(publication.track.attach());
+          }
+        });
 
-    room.on('participantConnected', participant => {
-      console.log(`A remote Participant connected: ${participant}`);
+       participant.on('trackSubscribed', track => {
+          document.getElementById('remote-media-div').appendChild(track.attach());
+        });
+      });
+
+    },
+      error => {
+      console.error(`Unable to connect to Room: ${error.message}`);
     });
-
-    room.participants.forEach(participant => {
-      participant.tracks.forEach(publication => {
-        if (publication.track) {
-          document.getElementById('remote-media-div').appendChild(publication.track.attach());
-        }
-      });
-    
-     participant.on('trackSubscribed', track => {
-        document.getElementById('remote-media-div').appendChild(track.attach());
-      });
-    });
-
-  }, 
-    error => {
-    console.error(`Unable to connect to Room: ${error.message}`);
-  });
+  }
 });
 
 
