7f70154bae2d911f9cf825f52283c37223122afa
Louis Renie <louis.renie@hotmail.com>
2020-09-03 11:55:00 +0200

    date en francais et bien affichées

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+  WEEKDAYS = ['Dim', 'Lun', 'Mar', 'Mer', 'Jeu', 'Ven', "Sam"]
+  MONTHNAMES = {
+    'January' => 'Jan',
+    'February' => 'Fev',
+    'March' => 'Mar',
+    'April' => 'Avr',
+    'May' => 'Mai',
+    'June' => 'Jui',
+    'July' => 'Juil',
+    'August' => 'Aoû',
+    'September' => 'Sep',
+    'October' => 'Oct',
+    'November' => 'Nov',
+    'December' => 'Dec',
+  }
+  def translate_date(date)
+    week_day = WEEKDAYS[date.wday]
+    monthname = MONTHNAMES[date.strftime('%B')]
+    return "#{week_day} #{date.day} #{monthname}"
+  end
 end
