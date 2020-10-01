3129ccd2f174c5d0bc8c27c8fd006e31302476dc
Corentin Lumineau <lumineau.corentin@gmail.com>
2020-10-01 09:26:16 +0200

    Implémentation de la page du tchat

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,16 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   resources :users, only: [:show, :index]
+  resources :chatrooms, only: :show do
+    resources :messages, only: :create
+  end
   get '/sessions/choice', to: 'sessions#choice'
   get '/sessions/fetch', to: 'sessions#fetch'
   get '/sessions/multi/new', to: 'sessions#new_multi', as: 'new_multi_session'
   get '/sessions/solo/new', to: 'sessions#new_solo', as: 'new_solo_session'
   post '/sessions', to: 'sessions#create_multi'
   get '/sessions/:id', to: 'sessions#show', as: 'my_session'
+
 
   # resources :sessions, only: [:new, :create]
 
