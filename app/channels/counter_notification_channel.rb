c1d71c66349d433e0246e65320fc04673475d75f
Eugenie Avedikian <eugenieavedikian@gmail.com>
2020-12-08 14:42:14 +0100

    suppression des channels inutiles

--- app/channels/counter_notification_channel.rb
+++
@@ -1,9 +0,0 @@
-class CounterNotificationChannel < ApplicationCable::Channel
-  def subscribed
-    stream_from "counter_notification:#{current_user.id}"
-  end
-
-  def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
-  end
-end
