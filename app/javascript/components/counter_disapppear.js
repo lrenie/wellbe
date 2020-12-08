d7f8eb520409c324c1b13c769d184ff8de348e8f
Eugenie Avedikian <eugenieavedikian@gmail.com>
2020-12-08 18:45:37 +0100

    debug signup

---
+++ app/javascript/components/counter_disapppear.js
@@ -0,0 +1,8 @@
+const counterDisappear = () => {
+  const list = document.querySelector('.dropdown-belt');
+  const counter = document.querySelector('.red-notif');
+
+  // list.addEventListener('hover', console.log('hoverer'))
+}
+
+export { counterDisappear }
