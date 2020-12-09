04255d7fe5d0410cfd1959eb1f88c24958189003
Corentin Lumineau <lumineau.corentin@gmail.com>
2020-12-09 13:31:57 +0100

    Mise à jour des associationns dans user

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,9 @@ class User < ApplicationRecord
   has_many :stats, dependent: :destroy
   has_many :session_participants, dependent: :destroy
   has_many :sessions, through: :session_participants, dependent: :destroy
+  has_many :chatrooms, dependent: :destroy
   has_many :chat_participants, dependent: :destroy
-  has_many :messages, through: :chat_participants, dependent: :destroy
+  has_many :messages, dependent: :destroy
   has_one_attached :avatar
   has_one_attached :cover
 
