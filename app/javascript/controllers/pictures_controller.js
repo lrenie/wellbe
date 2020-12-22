76cc9e78ad2410f66782a17832fe5de41090b53f
EugenieAv <59569740+EugenieAv@users.noreply.github.com>
2020-12-22 18:10:50 +0100

    Update pictures_controller.js

--- app/javascript/controllers/pictures_controller.js
+++ app/javascript/controllers/pictures_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "stimulus";
 import Rails from "@rails/ujs";
 
 export default class extends Controller {
-  static targets = [ "avatarImg", "avatarChargeBtn", "avatarForm", "avatarUser", "bobo" ]
+  static targets = [ "avatarImg", "avatarChargeBtn", "avatarForm", "avatarUser"]
 
   connect() {
   }
